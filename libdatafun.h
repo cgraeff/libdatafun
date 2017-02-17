1d73fdf525a827c3a89f702feb28fff37b29f4d9
Clebson Abati Graeff <cgraeff@utfpr.edu.br>
2017-02-17 14:57:11 -0200

    Add a header to include the whole library

---
+++ libdatafun.h
@@ -0,0 +1,17 @@
+//
+//  libdatafun.h
+//  libdatafun
+//
+//  Created by Clebson Graeff on 2017-02-14.
+//  Copyright © 2017 Clebson Graeff. All rights reserved.
+//
+
+#ifndef libdatafun_h
+#define libdatafun_h
+
+#include "headers/AuxiliaryFunctions.h"
+#include "headers/Derivatives.h"
+#include "headers/Integration.h"
+#include "RootFinding.h"
+
+#endif /* libdatafun_h */
